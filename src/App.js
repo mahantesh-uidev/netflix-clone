6df067d93dfee2fcd56e56e855bd36df2db991f9
mahantesh-uidev <mahantesh.uideveloper@gmail.com>
2024-02-25 17:45:16 +0530

    Sign In/Sign Up Form && Login Page and Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App">
-      <div className='text-3xl font-bold text-green-800'>Namaste React</div>
-    </div>
+    <Body />
   );
 }
 
